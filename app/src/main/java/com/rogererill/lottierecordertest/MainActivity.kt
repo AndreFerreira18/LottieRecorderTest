0994cf9d638230fd4fdf9cc5c5eb8c417fa5e1be
AndreFerreira18 <andrefferreira@outlook.pt>
2021-11-24 16:14:10 +0000

    Small indent corrections and background task for LottieCompositionFactory;

--- app/src/main/java/com/rogererill/lottierecordertest/MainActivity.kt
+++ app/src/main/java/com/rogererill/lottierecordertest/MainActivity.kt
@@ -3,47 +3,55 @@ package com.rogererill.lottierecordertest
 import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider.getUriForFile
+import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.LottieCompositionFactory
 import com.airbnb.lottie.LottieDrawable
+import com.airbnb.lottie.LottieResult
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    val textView: TextView = findViewById(R.id.tv_info)
-    val startButton: Button = findViewById(R.id.button_start)
+        val textView: TextView = findViewById(R.id.tv_info)
+        val startButton: Button = findViewById(R.id.button_start)
 
-    val lottieComposition = LottieCompositionFactory.fromRawResSync(this, R.raw.android_wave) // Make sure to call this on a background thread!
-    val lottieDrawable = LottieDrawable()
-    lottieDrawable.composition = lottieComposition.value
+        var lottieComposition: LottieResult<LottieComposition>?
+        Handler(Looper.getMainLooper()).postDelayed({
+            lottieComposition = LottieCompositionFactory.fromRawResSync(this, R.raw.android_wave)
 
-    val path = getExternalFilesDir(Environment.DIRECTORY_PICTURES) ?: File(cacheDir, Environment.DIRECTORY_PICTURES).apply { mkdirs() }
-    val videoFile = File(path, "lottie_in_video.mp4")
-    val recordingOperation = RecordingOperation(Recorder(videoOutput = videoFile), FrameCreator(lottieDrawable))
-    {
-      textView.text = getString(R.string.recording_finished)
-      openCreatedVideo(videoFile)
+            val lottieDrawable = LottieDrawable()
+            lottieDrawable.composition = lottieComposition?.value
+
+            val path = getExternalFilesDir(Environment.DIRECTORY_PICTURES) ?: File(cacheDir, Environment.DIRECTORY_PICTURES).apply { mkdirs() }
+            val videoFile = File(path, "lottie_in_video.mp4")
+            val recordingOperation = RecordingOperation(Recorder(videoOutput = videoFile), FrameCreator(lottieDrawable))
+            {
+                textView.text = getString(R.string.recording_finished)
+                openCreatedVideo(videoFile)
+            }
+
+            startButton.setOnClickListener {
+                textView.text = getString(R.string.recording)
+                recordingOperation.start()  // Make sure to call this on a background thread!
+            }
+        }, 0) // Make sure to call this on a background thread!
     }
 
-    startButton.setOnClickListener {
-      textView.text = getString(R.string.recording)
-      recordingOperation.start()  // Make sure to call this on a background thread!
+    private fun openCreatedVideo(videoFile: File) {
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        val uriForFile = getUriForFile(this, "com.rogererill.provider", videoFile)
+        intent.setDataAndType(uriForFile, "video/*")
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        startActivity(intent)
     }
-  }
-
-  private fun openCreatedVideo(videoFile: File) {
-    val intent = Intent()
-    intent.action = Intent.ACTION_VIEW
-    val uriForFile = getUriForFile(this, "com.rogererill.provider", videoFile)
-    intent.setDataAndType(uriForFile, "video/*")
-    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-    startActivity(intent)
-  }
 }
